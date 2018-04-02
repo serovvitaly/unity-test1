519dbb616bb31ff0e99799c75a844c208bc82a52
Vitaly Serov <vi.serov@teamber.ru>
2018-04-02 18:33:20 +0300

    First commit

---
+++ Assets/Scripts/FirstMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstMenu : MonoBehaviour
+{
+
+	public Button buttonCompany;
+	public Button buttonCancel;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Button btnCompany = buttonCompany.GetComponent<Button>();
+		btnCompany.onClick.AddListener(BtnCompanyOnClick);
+		
+		Button btnCancel = buttonCancel.GetComponent<Button>();
+		btnCancel.onClick.AddListener(BtnCancelOnClick);
+	}
+
+	void BtnCompanyOnClick()
+	{
+		Debug.Log("Нажата кнопка `Компания`");
+	}
+
+	void BtnCancelOnClick()
+	{
+		Debug.Log("Нажата кнопка `Покинуть игру`");
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+	public Button FirstButton;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		//
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
